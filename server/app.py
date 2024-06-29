c352ba89f6c643225610edd1790f86a8251ad3f2
ALLAN <allankipr6@gmail.com>
2024-06-29 14:42:15 +0300

    completed lab with all test pasing

--- server/app.py
+++ server/app.py
@@ -23,6 +23,44 @@ api = Api(app)
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
+@app.route("/restaurants")
+def get_restaurants():
+    restaurants=[]
+    for restaurant in Restaurant.query.all():
+        restaurants.append(restaurant.to_dict(rules=["-restaurant_pizzas"]))
+    return make_response(restaurants, 200)
+
+@app.route("/restaurants/<int:id>", methods=["GET", "DELETE"])
+def restaurant(id):
+    restaurant = Restaurant.query.filter_by(id=id).first()
+    if request.method == "GET":
+        if not restaurant:
+            return make_response({"error": "Restaurant not found"}, 404)
+        return make_response(restaurant.to_dict(), 200)
+    if request.method == "DELETE": 
+        if not restaurant:
+            return make_response({"error": "Restaurant not found"}, 404)
+        db.session.delete(restaurant)
+        db.session.commit()
+        return make_response(restaurant.to_dict(), 204)
+    
+@app.route("/pizzas")
+def get_pizzas():
+    pizzas=[]
+    for pizza in Pizza.query.all():
+        pizzas.append(pizza.to_dict(rules=["-restaurant_pizzas"]))
+    return make_response(pizzas, 200)
+@app.route("/restaurant_pizzas", methods=["POST"])
+def create_restaurant_pizza():
+    data = request.json
+    try:
+        rp = RestaurantPizza(price=data.get('price'), restaurant_id=data.get('restaurant_id'), pizza_id=data.get('pizza_id'))
+        db.session.add(rp)
+        db.session.commit()
+        return rp.to_dict(), 201
+    except Exception as e:
+        print(e)
+        return {'errors':["validation errors"]}, 400
 
 
 if __name__ == "__main__":
